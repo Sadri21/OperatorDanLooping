751bb25dedb771efe9294a7c9006eb93c17bee2b
sadri21 <sadriashari21@gmail.com>
2015-12-23 20:45:13 +0700

    Operator Aritmatika

---
+++ OperatorAritmatika.py
@@ -0,0 +1,9 @@
+print "belajar operator aritmatika"
+a = 10
+b = 2
+print a + b #Pejumlahan
+print a - b #Pengurangan
+print a / b #Pembagian
+print a * b #Pengalian
+print a ** b #Pemanggkatan
+print a % b #SisaBagi 
